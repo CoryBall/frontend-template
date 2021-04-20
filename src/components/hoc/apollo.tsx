369de3e0e943610ef53df8e36e5bc04692b71eef
Cory Ball <cory.ball@altiview.us>
2021-04-20 04:55:53 -0500

    context init

--- src/components/hoc/apollo.tsx
+++ src/components/hoc/apollo.tsx
@@ -1,82 +1,75 @@
-import React from 'react';
+import React from 'react'
 import {
   ApolloClient,
   ApolloLink,
   HttpLink,
   InMemoryCache,
   NormalizedCacheObject,
-} from '@apollo/client';
-import apolloLogger from 'apollo-link-logger';
-import { onError } from '@apollo/client/link/error';
-import fetch from 'isomorphic-unfetch';
-import merge from 'deepmerge';
-import isEqual from 'lodash/isEqual';
+} from '@apollo/client'
+import apolloLogger from 'apollo-link-logger'
+import { onError } from '@apollo/client/link/error'
+import fetch from 'isomorphic-unfetch'
+import merge from 'deepmerge'
+import isEqual from 'lodash/isEqual'
 import AppConfig from '../../config/app.config'
 
-export const APOLLO_STATE_PROP_NAME = '__APOLLO_STATE__';
+export const APOLLO_STATE_PROP_NAME = '__APOLLO_STATE__'
 
-let apolloClient: ApolloClient<NormalizedCacheObject>;
+let apolloClient: ApolloClient<NormalizedCacheObject>
 
 function createApolloClient(
-  accessToken?: string,
-  refreshToken?: string
+  bearer?: string
 ): ApolloClient<NormalizedCacheObject> {
   return new ApolloClient({
     ssrMode: typeof window === 'undefined',
-    link: createLink(accessToken, refreshToken),
+    link: createLink(bearer),
     cache: new InMemoryCache(),
     connectToDevTools: true,
-  });
+  })
 }
 
-function createHttpLink(accessToken?: string, refreshToken?: string): HttpLink {
+function createHttpLink(bearer?: string): HttpLink {
   return new HttpLink({
     uri: AppConfig.GraphQLEndpoint,
     credentials: 'same-origin',
     fetch,
     headers: {
-      'x-access-token': !accessToken ? '' : accessToken,
-      'x-refresh-token': !refreshToken ? '' : refreshToken,
+      Authorization: !bearer ? '' : `Bearer: ${bearer}`,
     },
-  });
+  })
 }
 
-export function createLink(
-  accessToken?: string,
-  refreshToken?: string
-): ApolloLink {
+export function createLink(bearer?: string): ApolloLink {
   return ApolloLink.from([
     apolloLogger,
     createErrorLink(),
-    createHttpLink(accessToken, refreshToken),
-  ]);
+    createHttpLink(bearer),
+  ])
 }
 
 function createErrorLink(): ApolloLink {
   return onError(function ({ graphQLErrors, networkError }) {
     if (graphQLErrors) {
       graphQLErrors.forEach(({ message }) => {
-        console.error(message);
-      });
+        console.error(message)
+      })
     }
     if (networkError) {
-      console.error(networkError.message);
+      console.error(networkError.message)
     }
-  });
+  })
 }
 
 export function initializeApollo(
-  accessToken?: string,
-  refreshToken?: string,
+  bearer?: string,
   initialState?: NormalizedCacheObject
 ): ApolloClient<NormalizedCacheObject> {
-  const _apolloClient =
-    apolloClient ?? createApolloClient(accessToken, refreshToken);
+  const _apolloClient = apolloClient ?? createApolloClient(bearer)
   // If your page has Next.js data fetching methods that use Apollo Client,
   // the initial state gets hydrated here
   if (initialState) {
     // Get existing cache, loaded during client side data fetching
-    const existingCache = _apolloClient.extract();
+    const existingCache = _apolloClient.extract()
 
     // Merge the existing cache into data passed from getStaticProps/getServerSideProps
     const data = merge(initialState, existingCache, {
@@ -87,18 +80,18 @@ export function initializeApollo(
           sourceArray.every((s) => !isEqual(d, s))
         ),
       ],
-    });
+    })
 
     // Restore the cache with the merged data
-    _apolloClient.cache.restore(data);
+    _apolloClient.cache.restore(data)
   }
 
   // For SSG and SSR always create a new Apollo Client
-  if (typeof window === 'undefined') return _apolloClient;
+  if (typeof window === 'undefined') return _apolloClient
 
   // Create the Apollo Client once in the client
-  if (!apolloClient) apolloClient = _apolloClient;
-  return _apolloClient;
+  if (!apolloClient) apolloClient = _apolloClient
+  return _apolloClient
 }
 
 // This is only for SSG using getStaticProps.
@@ -110,14 +103,14 @@ export function addApolloState(
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
 ): any {
   if (pageProps?.props) {
-    pageProps.props[APOLLO_STATE_PROP_NAME] = client.cache.extract();
+    pageProps.props[APOLLO_STATE_PROP_NAME] = client.cache.extract()
   }
 
-  return pageProps;
+  return pageProps
 }
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types, @typescript-eslint/no-explicit-any
 export function useApollo(pageProps: any): ApolloClient<NormalizedCacheObject> {
-  const state = pageProps[APOLLO_STATE_PROP_NAME];
-  return React.useMemo(() => initializeApollo(state), [state]);
+  const state = pageProps[APOLLO_STATE_PROP_NAME]
+  return React.useMemo(() => initializeApollo(state), [state])
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,21 +1,28 @@
-import React from 'react';
+import React from 'react'
 import { NextPage } from 'next'
 import { AppProps } from 'next/app'
-import { ApolloClient, ApolloProvider, NormalizedCacheObject } from '@apollo/client'
-import {AnimateSharedLayout} from 'framer-motion'
-import { useApollo } from '../components/hoc/apollo'
-
+import {
+  ApolloClient,
+  ApolloProvider,
+  NormalizedCacheObject,
+} from '@apollo/client'
+import { AnimateSharedLayout } from 'framer-motion'
+import { useApollo, AppProvider } from '../components/hoc'
 
 const MyApp: NextPage<AppProps> = (props: AppProps) => {
-  const { Component, pageProps } = props;
-  const apolloClientRef = React.useRef<ApolloClient<NormalizedCacheObject>>(useApollo(pageProps));
+  const { Component, pageProps } = props
+  const apolloClientRef = React.useRef<ApolloClient<NormalizedCacheObject>>(
+    useApollo(pageProps)
+  )
 
   return (
     <div className="flex flex-col h-screen w-screen">
       <ApolloProvider client={apolloClientRef.current}>
-        <AnimateSharedLayout>
-          <Component {...pageProps} />
-        </AnimateSharedLayout>
+        <AppProvider apollo={apolloClientRef.current}>
+          <AnimateSharedLayout>
+            <Component {...pageProps} />
+          </AnimateSharedLayout>
+        </AppProvider>
       </ApolloProvider>
     </div>
   )
