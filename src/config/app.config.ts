5b084d8f9ff370dd9a5fced3d29365f3e5154a7f
Cory Ball <cory.ball@altiview.us>
2021-04-19 21:47:09 -0500

    init

---
+++ src/config/app.config.ts
@@ -0,0 +1,11 @@
+class AppConfig {
+  private static DevServer = 'localhost:4000';
+  private static ProdServer = 'api.domain';
+
+  static IsProd: boolean = process.env.NODE_ENV === 'production';
+  
+  static GraphQLEndpoint = `http${AppConfig.IsProd && 's'}://${AppConfig.IsProd ? AppConfig.ProdServer : AppConfig.DevServer}/graphql`;
+  static SubscriptionEndpoint = `ws://${AppConfig.IsProd ? AppConfig.ProdServer : AppConfig.DevServer}/subscriptions`;
+}
+
+export default AppConfig;
